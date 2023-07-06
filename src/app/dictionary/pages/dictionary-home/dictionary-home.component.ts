844c4af76f62722955dcbe48d04ed8d695de0e1f
Gonzalo Ferreyra <gonferreyra@gmail.com>
2023-07-06 11:30:27 -0300

    very simple styles were added

--- src/app/dictionary/pages/dictionary-home/dictionary-home.component.ts
+++ src/app/dictionary/pages/dictionary-home/dictionary-home.component.ts
@@ -6,26 +6,23 @@ import { SearchResponse } from '../../interfaces/searchResponse';
 @Component({
   selector: 'app-dictionary-home',
   templateUrl: './dictionary-home.component.html',
-  styles: [],
+  styleUrls: ['./dictionary-home.component.css'],
 })
 export class DictionaryHomeComponent {
   public searchWord: SearchResponse[] = [];
   public searchValue = new FormControl('');
-
   constructor(private dictionaryService: DictionaryService) {}
-
   getSearchValue(): void {
     // console.log(this.searchValue.value);
-    if (this.searchWord.length < 1) return; // agregar error, ver observables
+    if (this.searchValue.value === '') return; // agregar mensage de error
     if (this.searchWord.length >= 1) this.searchWord = [];
-
     this.dictionaryService
       .searchByWord(this.searchValue.value)
       .subscribe((word) => {
         this.searchWord.push(word[0]);
-        console.log(this.searchWord);
+        // console.log(this.searchWord);
       });
-
     // reset form
+    this.searchValue.reset();
   }
 }
