d6b1f627d4cc930ba6009a83fc391da902d32c02
Gonzalo Ferreyra <gonferreyra@gmail.com>
2023-07-06 11:29:43 -0300

    delete empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { AppComponent } from './app.component';
 import { DictionaryHomeComponent } from './dictionary/pages/dictionary-home/dictionary-home.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { SearchBoxComponent } from './dictionary/components/search-box/search-box.component';
 
 @NgModule({
-  declarations: [AppComponent, DictionaryHomeComponent, SearchBoxComponent],
+  declarations: [AppComponent, DictionaryHomeComponent],
   imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/dictionary/components/search-box/search-box.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'dictionary-search-box',
-  templateUrl: './search-box.component.html',
-  styles: [],
-})
-export class SearchBoxComponent {}
